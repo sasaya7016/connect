a4ed51d29b4aa20771217b2e3af4743937e176d3
sasaya7016 <sasaya7016@outlook.jp>
2020-06-12 00:18:44 +0900

    Update Tweet Controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,9 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(10)
   end
 
   def new
@@ -13,11 +16,9 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -34,6 +35,14 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id]) 
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
