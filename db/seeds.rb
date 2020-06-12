8cad083f2bdea184ad6fc302304895408ffc6b6e
sasaya7016 <sasaya7016@outlook.jp>
2020-06-12 20:53:52 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  nickname:  "管理者",
+  email: "admin@gmail.com",
+  password:  "00000000",
+  password_confirmation: "00000000",
+  # admin: true
+)
+
+User.create!(
+  nickname:  "TEST1",
+  email: "test1@gmail.com",
+  password:  "11111111",
+  password_confirmation: "11111111",
+)
+
+User.create!(
+  nickname:  "TEST2",
+  email: "test2@gmail.com",
+  password:  "22222222",
+  password_confirmation: "22222222",
+)
