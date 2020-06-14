266a2350d6cdfe59ebdf408396c2da83750dcec7
sasaya7016 <sasaya7016@outlook.jp>
2020-06-15 03:38:00 +0900

    Update userdb deploy編集

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_06_14_054043) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
