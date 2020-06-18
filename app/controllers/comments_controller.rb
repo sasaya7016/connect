24f3f8222a46b4c4df0de017d8f20a250a23f317
sasaya7016 <sasaya7016@outlook.jp>
2020-06-18 14:36:10 +0900

    非同期解除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    respond_to do |format|
-      format.html { redirect_to tweet_path(params[:tweet_id])}
-      format.json
-    end
+    redirect_to "/tweets/#{comment.tweet.id}"
+    # respond_to do |format|
+    #   format.html { redirect_to tweet_path(params[:tweet_id])}
+    #   format.json
+    # end
   end
 
   def destroy
