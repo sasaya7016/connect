42c128cbc9f0108f7be462d5cee1d69a95d37a65
sasaya7016 <sasaya7016@outlook.jp>
2020-06-20 23:01:09 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(
-  name: "管理者",
-  nickname:  "管理者",
-  email: "admin@gmail.com",
+  name: "テスト0",
+  nickname:  "テスト0",
+  email: "test0@gmail.com",
   password:  "00000000",
   password_confirmation: "00000000",
   profile:"ここの管理者をしております。よろしくお願いいたします。",
@@ -33,3 +33,31 @@ User.create!(
   password_confirmation: "22222222",
   profile:"初めまして、テスト２です。よろしくお願いいたします。",
 )
+
+User.create!(
+  name: "テスト3",
+  nickname:  "テスト3",
+  email: "test3@gmail.com",
+  password:  "33333333",
+  password_confirmation: "33333333",
+  profile:"初めまして、テスト３です。よろしくお願いいたします。",
+)
+
+User.create!(
+  name: "テスト4",
+  nickname:  "テスト4",
+  email: "test4@gmail.com",
+  password:  "44444444",
+  password_confirmation: "44444444",
+  profile:"初めまして、テスト４です。よろしくお願いいたします。",
+)
+
+User.create!(
+  name: "テスト5",
+  nickname:  "テスト5",
+  email: "test5@gmail.com",
+  password:  "55555555",
+  password_confirmation: "55555555",
+  profile:"初めまして、テスト５です。よろしくお願いいたします。",
+)
+
